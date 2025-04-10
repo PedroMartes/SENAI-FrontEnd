143c1d611639b60bf3f8e1803185bcdc91108108
PedroMartes <pedromartes.apps@gmail.com>
2025-04-10 08:52:44 -0300

    Adicionando estrutura html, css e Javascript

---
+++ 10-04-2025/script.js
@@ -0,0 +1,27 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "Corolla",
+        year: 2020
+    },
+    {
+        brand: "Honda",
+        model: "Civic",
+        year: 2019
+    },
+    {
+        brand: "Ford",
+        model: "Fiesta",
+        year: 2014
+    },
+    {
+        brand: "Ford",
+        model: "Focus",
+        year: 2018
+    }, 
+];
+
+console.log(cars [0].brand); 
+console.log(cars [2].model);
+
+ 
